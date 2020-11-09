8fef012dae36ebdfd2a9ff497e4c70a302263fcf
neha <nzaveri@equalexperts.com>
2020-11-10 01:25:27 +0530

    Added DynamoDB implementation n test for the same

--- src/main/java/com/paathshala/flight/flightservice/repository/FlightRepository.java
+++ src/main/java/com/paathshala/flight/flightservice/repository/FlightRepository.java
@@ -1,66 +1,12 @@
 package com.paathshala.flight.flightservice.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
-
 import com.paathshala.flight.flightservice.model.Flight;
-import com.paathshala.flight.flightservice.model.Message;
-import com.paathshala.flight.flightservice.model.MessagePublish;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public class FlightRepository {
+import java.util.Optional;
 
-	private static List<Flight> flights = new ArrayList<>();
-	private static int idCounter = 0;
-
-	private RestTemplate restTemplate;
-	
-	@Value("${message.service.url}")
-    private String messageServiceUrl;
-
-	@PostConstruct
-	public void init() {
-		restTemplate = new RestTemplate();
-	}
-
-	static {
-		flights.add(new Flight(++idCounter, "AIR INDIA", "Mumbai", "Pune"));
-		flights.add(new Flight(++idCounter, "VISTARA", "Ahmedabad", "Pune"));
-		flights.add(new Flight(++idCounter, "AIR INDIA", "Ooty", "Chennai"));
-		flights.add(new Flight(++idCounter, "INDIGO", "Delhi", "Bangaluru"));
-		flights.add(new Flight(++idCounter, "AIR INDIA", "Kolkata", "Nagpur"));
-		flights.add(new Flight(++idCounter, "VISTARA", "Hyderabad", "Patna"));
-		flights.add(new Flight(++idCounter, "AIR INDIA", "Rajkot", "Mysore"));
-		flights.add(new Flight(++idCounter, "INDIGO", "Jaipur", "Bhubaneshwar"));
-	}
-
-	private HttpHeaders getHttpHeaders() {
-		HttpHeaders headers = new HttpHeaders();
-		List<MediaType> acceptableMediaTypes = new ArrayList<>();
-		acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
-		headers.setAccept(acceptableMediaTypes);
-		return headers;
-	}
-
-	public List<Flight> getAllFlights() {
-		return flights;
-	}
-
-	public MessagePublish bookFlight(Message request) {
-		HttpEntity<MessagePublish> entity = new HttpEntity<>(getHttpHeaders());
-		ResponseEntity<MessagePublish> response = 
-				restTemplate.postForEntity(messageServiceUrl, request, MessagePublish.class);
-		return response.getBody();
-	}
-
+@EnableScan
+public interface FlightRepository extends CrudRepository<Flight, String> {
+    Optional<Flight> findById(String id);
 }
--- src/main/java/com/paathshala/flight/flightservice/service/FlightService.java
+++ src/main/java/com/paathshala/flight/flightservice/service/FlightService.java
@@ -1,29 +1,54 @@
 package com.paathshala.flight.flightservice.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Service;
-
 import com.paathshala.flight.flightservice.model.Booking;
 import com.paathshala.flight.flightservice.model.Flight;
 import com.paathshala.flight.flightservice.model.Flights;
 import com.paathshala.flight.flightservice.model.Message;
 import com.paathshala.flight.flightservice.model.MessagePublish;
 import com.paathshala.flight.flightservice.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class FlightService {
 
 	@Autowired
-	private FlightRepository flightRepository;
+	private FlightRepository repository;
 
 	@Autowired
 	private Environment env;
 
+	private RestTemplate restTemplate;
+
+	@Value("${message.service.url}")
+	private String messageServiceUrl;
+
+	@PostConstruct
+	public void init() {
+		restTemplate = new RestTemplate();
+	}
+
+
 	public Flights getAllFlights() {
-		List<Flight> flightList = flightRepository.getAllFlights();
+		Iterable<Flight> flightsSaved = repository
+				.findAll();
+		List<Flight> flightList = StreamSupport.stream(flightsSaved
+				.spliterator(), false)
+				.collect(Collectors.toList());
 		Flights flights = new Flights();
 		flights.setFlights(flightList);
 		flights.setHostName(
@@ -36,12 +61,27 @@ public class FlightService {
 		Message request = new Message();
 		request.setFlight(flight);
 		request.setEmailId("azaveri7@gmail.com");
-		MessagePublish response = flightRepository.bookFlight(request);		
+		MessagePublish response = bookFlight(request);
 		booking.setFlight(flight);
 		booking.setMessage("SUCCESS");
 		booking.setHostName(
 				env.getProperty("local.server.host") + " is running on " + env.getProperty("local.server.port"));
 		return booking;
+	}
+
+	private MessagePublish bookFlight(Message request) {
+		HttpEntity<MessagePublish> entity = new HttpEntity<>(getHttpHeaders());
+		ResponseEntity<MessagePublish> response =
+				restTemplate.postForEntity(messageServiceUrl, request, MessagePublish.class);
+		return response.getBody();
+	}
+
+	private HttpHeaders getHttpHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		List<MediaType> acceptableMediaTypes = new ArrayList<>();
+		acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+		headers.setAccept(acceptableMediaTypes);
+		return headers;
 	}
 
 }
